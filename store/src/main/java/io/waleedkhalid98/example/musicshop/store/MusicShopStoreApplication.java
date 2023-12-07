a608e7e0b1dcdb37216c359b021217bc814a9e2c
Waleedkhalid98 <waleed.khalid@studenti.unicam.it>
2023-12-07 21:50:33 +0100

    sync

---
+++ store/src/main/java/io/waleedkhalid98/example/musicshop/store/MusicShopStoreApplication.java
@@ -0,0 +1,13 @@
+package io.waleedkhalid98.example.musicshop.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicShopStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicShopStoreApplication.class, args);
+	}
+
+}
