bf89ad9d757237d547b8ee761e2046b27241e28f
Waleedkhalid98 <waleed.khalid@studenti.unicam.it>
2023-12-23 12:23:03 +0100

    login controllor signUp

---
+++ store/src/main/java/io/waleedkhalid98/example/musicshop/store/controller/LoginController.java
@@ -0,0 +1,22 @@
+package io.waleedkhalid98.example.musicshop.store.controller;
+
+//import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin(origins = {"http://localhost:4200", "http://127.0.0.1:4200"})
+@RequestMapping(value = "/account")
+//@Slf4j
+public class LoginController {
+
+    @PostMapping(path = "/sign-up")
+    public ResponseEntity<Void> signUp(@RequestBody String signUp){
+//        log.info("Starting signup for: {}", signUp);
+//        log.info("Finish signup for: {}", signUp);
+        System.out.println(signUp);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+}
