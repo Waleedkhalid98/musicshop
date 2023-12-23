0bdf2dff9dea8e8f768b99f182e10d0b38c1b883
Waleedkhalid98 <waleed.khalid@studenti.unicam.it>
2023-12-23 12:20:09 +0100

    poms updates

--- store/src/test/java/io/waleedkhalid98/example/musicshop/store/MusicShopStoreApplicationTests.java
+++ store/src/test/java/io/waleedkhalid98/example/musicshop/store/MusicShopStoreApplicationTests.java
@@ -1,13 +1,13 @@
-package io.waleedkhalid98.example.musicshop.store;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class MusicShopStoreApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package io.waleedkhalid98.example.musicshop.store;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class MusicShopStoreApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
